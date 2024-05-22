6125da791096c35626346dfb11bb7cff7b7956d7
dziubenzo <dziubenzo@gmail.com>
2024-05-22 15:06:01 +0200

    Build POST /users/login route

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,17 +10,23 @@ import {
 } from '../config/middleware.js';
 import { getFirstErrorMsg } from '../config/helpers.js';
 
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+
 // @desc    Get all users
 // @route   GET /users
-export const getAllUsers = asyncHandler(async (req, res, next) => {
-  const allUsers = await User.find({}, '-password').exec();
+export const getAllUsers = [
+  passport.authenticate('jwt', { session: false }),
+  asyncHandler(async (req, res, next) => {
+    const allUsers = await User.find({}, '-password').exec();
 
-  if (allUsers.length === 0) {
-    return res.status(404).json(allUsers);
-  }
+    if (allUsers.length === 0) {
+      return res.status(404).json(allUsers);
+    }
 
-  return res.json(allUsers);
-});
+    return res.json(allUsers);
+  }),
+];
 
 // @desc    Create user
 // @route   POST /users
@@ -81,5 +87,56 @@ export const createUser = [
     }).save();
 
     return res.json('User created successfully!');
+  }),
+];
+
+// @desc    Log in user
+// @route   POST /users/login
+export const loginUser = [
+  // Validate and sanitise login form fields
+  body('username')
+    .trim()
+    .isLength({ min: 3, max: 16 })
+    .withMessage('Username must contain between 3 and 16 characters'),
+  body('password')
+    .trim()
+    .isLength({ min: 3, max: 16 })
+    .withMessage('Password must contain between 3 and 16 characters'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Return the first validation error message if there are any errors
+      const firstErrorMsg = getFirstErrorMsg(errors);
+      return res.status(400).json(firstErrorMsg);
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Get user from the DB
+    const user = await User.findOne({ username }).exec();
+
+    // Return error message if no user found
+    if (!user) {
+      return res.status(401).json('Invalid username and/or password');
+    }
+
+    // Compare passwords
+    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+    // Return error message if passwords do not match
+    if (!passwordsMatch) {
+      return res.status(401).json('Invalid username and/or password');
+    }
+
+    // Create token valid for 3 days if login credentials are valid
+    // Use user's ID as payload
+    const options = { expiresIn: '3 days' };
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, options);
+
+    // Return token
+    return res.json(token);
   }),
 ];
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { getAllUsers, createUser } from '../controllers/userController.js';
+import {
+  getAllUsers,
+  createUser,
+  loginUser,
+} from '../controllers/userController.js';
 
 const router = Router();
 
 router.get('/', getAllUsers);
 
 router.post('/', createUser);
+
+router.post('/login', loginUser);
 
 export default router;
